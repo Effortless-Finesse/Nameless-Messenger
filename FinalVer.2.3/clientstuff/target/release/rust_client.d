02aa4e59483a1eb030329df8793f86df92220a83
jokumon <70261769+jokumon@users.noreply.github.com>
2025-04-26 00:55:32 +0500

    Added encryption in finalver

---
+++ FinalVer.2.3/clientstuff/target/release/rust_client.d
@@ -0,0 +1,1 @@
+/home/ardoe/unistuff/loserproj/Nameless-Messenger/tempworkah/clientstuff/target/release/rust_client: /home/ardoe/unistuff/loserproj/Nameless-Messenger/tempworkah/clientstuff/src/clientmain.rs
